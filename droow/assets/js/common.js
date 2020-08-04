b4f96cbcf1cb6596c297065fc639eb40fa218de2
Tim <59957508+Tim1703@users.noreply.github.com>
2020-08-04 19:06:15 +0300

    v3.1

---
+++ droow/assets/js/common.js
@@ -0,0 +1,16 @@
+//Ajax form
+
+$("form.row") .submit(function(){
+    var th = $(this);
+    $.ajax({
+        type: "POST",
+        url: "/mail.php",
+        data: th.serialize()
+    }).done(function(){
+        alert("Thank You");
+        setTimeout(function(){
+            th.trigger("reset");
+        },3000);
+    });
+    return false;
+});
